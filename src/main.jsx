44f3bc0f5bb62447fec1563fb524296fe2613d39
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-05-31 10:22:48 -0300

    globais css, arranjo pastas e fontes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from './../layout/layout'
-import Contact from './../pages/contact'
-import './../styles/global.scss'
+import Layout from './layout/layout'
+import Contact from './pages/contact'
+import './styles/global.scss'
 
 //aqui será configurada as rotas do site
 ReactDOM.createRoot(document.getElementById('root')).render(
