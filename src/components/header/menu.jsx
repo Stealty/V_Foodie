68986b87842408fb530c4b88bbf4b7f4ca34a8a5
Orion Antonucci <wagantonucci@hotmail.com>
2022-06-01 14:26:14 -0300

    animation start and include Social in header

--- src/components/header/menu.jsx
+++ src/components/header/menu.jsx
@@ -3,12 +3,19 @@ import './menu.scss'
 
 const Menu = () => {
     return <nav>
-        <ul className="Menu">
+        <ul className="Menu-Hamburguer">
             <li className="Menu-Top"></li>
             <li className="Menu-Middle"></li>
             <li className="Menu-Bottom"></li>
         </ul>
+        <ul className="Menu Menu-Slider-On">
+            <li>Home</li>
+            <li>Recipes</li>
+            <li>Blogaaaa</li>
+            <li>About Us</li>
+            <li>Contact</li>
+        </ul>
     </nav>
   };
-  
+
   export default Menu;
