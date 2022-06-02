ce94ff5bc2dbf5ce454c16efbadabe7b2b71a830
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-06-02 13:05:30 -0300

    acrescentado imagens

--- src/components/subscriptionCard/subscriptionCard.jsx
+++ src/components/subscriptionCard/subscriptionCard.jsx
@@ -5,14 +5,51 @@ import styles from "./subscriptionCard.module.scss";
 const SubscriptionCard = () => {
   return (
     <div className={styles["card"]}>
-      <h2 className={styles["card__title"]}>Card Title</h2>
+      <h2 className={styles["card__title"]}>Deliciousness to your inbox</h2>
       <p className={styles["card__description"]}>
         Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqut enim ad minim
       </p>
-      <div className={styles["card__button"]}>
-        <SubmitButton />
-      </div>
+      <form className={styles["form"]}>
+        <input
+          className={styles["form__input"]}
+          type="text"
+          placeholder="Your email address..."
+        />
+        <div className={styles["form__button"]}>
+          <SubmitButton />
+        </div>
+      </form>
+      <img
+        className={
+          styles["card__backgroundImage"] +
+          " " +
+          styles["card__backgroundImage--left"]
+        }
+        src="images/forks-and-salad.png"
+        alt=""
+        loading="lazy"
+      />
+      <img
+        className={
+          styles["card__backgroundImage"] +
+          " " +
+          styles["card__backgroundImage--float"]
+        }
+        src="images/rucula.png"
+        alt=""
+        loading="lazy"
+      />
+      <img
+        className={
+          styles["card__backgroundImage"] +
+          " " +
+          styles["card__backgroundImage--right"]
+        }
+        src="images/avocado-salad.png"
+        alt=""
+        loading="lazy"
+      />
     </div>
   );
 };
