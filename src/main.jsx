b0e8cb57a904db3a0bceb4d07bfbdd60a80c07a5
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-06-02 11:23:37 -0300

    Remocao de useRef e refs

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Layout from './layout/layout'
-import Contact from './pages/contact'
-import './styles/global.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Layout from "./layout/layout";
+import Contact from "./pages/contact";
+import "./styles/global.scss";
 
 //aqui será configurada as rotas do site
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Layout>
       <Contact />
     </Layout>
   </React.StrictMode>
-)
+);
