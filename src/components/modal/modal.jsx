d0bee697f932604478754190285021aedaa27c1f
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-06-10 09:07:18 -0300

    colocado tags apropriadas para o modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useId } from "react";
 import styles from "./modal.module.scss";
 
 function Modal({ message, onClose, show }) {
-  useEffect(() => {
-    if (show) {
-      setTimeout(() => {
-        onClose();
-      }, 3000);
-    }
-  }, [show]);
+  const descriptionID = useId();
 
   //add an event listener to the window to close the modal on any button press
   useEffect(() => {
-    window.addEventListener("keyup", onClose);
+    const closeOnEsc = (e) => {
+      if (e.keyCode === 27) {
+        onClose();
+      }
+    };
+    window.addEventListener("keyup", closeOnEsc);
     return () => {
-      window.removeEventListener("keyup", onClose);
+      window.removeEventListener("keyup", closeOnEsc);
     };
   }, [show]);
 
   return (
     <>
       {show && (
-        <div className={styles.modalCard} onClick={onClose}>
-          <p>{message}</p>
+        <div
+          className={styles.modalCard}
+          onClick={onClose}
+          aria-label="Request result"
+          aria-describedby={descriptionID}
+          aria-modal="true"
+          role="dialog"
+        >
+          <p id={descriptionID}>{message}</p>
         </div>
       )}
       {show && <div className={styles.modalBackdrop} onClick={onClose}></div>}
