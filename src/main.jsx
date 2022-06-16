e72e6bb7d9ad29c2ac387dfc8e8a2c1b5db2b0a9
Orion Antonucci <wagantonucci@hotmail.com>
2022-06-16 10:46:34 -0300

    create Router and Blog page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Layout from "./layout/layout";
 import Contact from "./pages/contact";
+import BlogList from "./pages/bloglist";
 import "./styles/global.scss";
 
 //aqui será configurada as rotas do site
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Layout>
-      <Contact />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Blog" element={<BlogList />} />
+      </Routes>
+      </BrowserRouter>
     </Layout>
   </React.StrictMode>
 );
