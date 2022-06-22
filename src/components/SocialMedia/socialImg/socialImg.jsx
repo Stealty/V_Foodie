ac82ecbb2423d33c8bf7474ff7d8a7be05adcc29
Orion Antonucci <wagantonucci@hotmail.com>
2022-06-22 18:03:19 -0300

    convert images to SVG

--- src/components/SocialMedia/socialImg/socialImg.jsx
+++ src/components/SocialMedia/socialImg/socialImg.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./socialImg.module.scss";
 
-const SocialImg = (props) => {
-  return (
-    <a href="#">
-      <img src={props.img} />
+const SocialImg = ({ children }) => {
+  return <svg className={styles.Svg}  viewBox="0 0 47 36">
+    <a className={styles.Link} href="#">
+      {children}
     </a>
-  );
-};
+  </svg>
+}
 
 export default SocialImg;
