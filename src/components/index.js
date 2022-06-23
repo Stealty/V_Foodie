5fbec7efae4c5e1708f8273ba051261001fda895
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-06-22 22:48:11 -0300

    Criação do Article Filter versão mobile

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Section } from "./section/section";
 export { default as SkipToContentButton } from "./skipToContentButton/skipToContentButton";
 export { default as SubmitButton } from "./submitButton/submitButton";
 export { default as NavItem } from "./navItem/navItem";
+export { default as ArticleFilter } from "./ArticleFilter/ArticleFilter";
--- src/pages/bloglist.jsx
+++ src/pages/bloglist.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Section } from "@/components";
 import { SubscriptionCard } from "@/features";
+import ArticleFilter from "../components/ArticleFilter/ArticleFilter";
 
 const BlogList = () => {
-    return <Section arialabel="Subscription Card">
-    <SubscriptionCard />
+  return (
+    <Section arialabel="Subscription Card">
+      <ArticleFilter />
+      <SubscriptionCard />
     </Section>
-}
+  );
+};
 
 export default BlogList;
