389a0695621fba418b9ba6773bfe7888e1ebc050
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-06-23 10:30:38 -0300

    implement grid for blogarticles components

---
+++ src/features/blogArticles/blogArticles.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./blogArticles.module.scss";
+
+const BlogArticles = () => {
+  return (
+    <>
+      <h1 className={styles["blogArticles__title"]}>Blog {`&`} Article</h1>
+      <p className={styles["blogArticles__subtitle"]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore
+      </p>
+      <div className={styles["blogArticles__wrapper"]}>
+        <div className={styles["blogArticles__searchBar"]}>Search Bar</div>
+        <div className={styles["blogArticles__articlesStack"]}>
+          Article Stack
+        </div>
+        <div className={styles["blogArticles__recipesStack"]}>
+          Recipes Stack
+        </div>
+        <div className={styles["blogArticles__ads"]}>Ads</div>
+        <div className={styles["blogArticles__pageSelector"]}>
+          Page Selector
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogArticles;
