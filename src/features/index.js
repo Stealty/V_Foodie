61e39db5eb3d0a648ace58cbe006028ee033b007
Orion Antonucci <wagantonucci@hotmail.com>
2022-06-24 13:17:11 -0300

    change navigation dir

--- src/features/index.js
+++ src/features/index.js
@@ -2,6 +2,5 @@ export { default as Carousel } from "./Carousel/Carousel";
 export { default as ContactForm } from "./contactForm/contactForm";
 export { default as Footer } from "./footer/footer";
 export { default as Header } from "./header/header";
-export { default as Navigation } from "./navigation/navigation";
 export { default as NotFound} from "./notfound/notfound";
 export { default as SubscriptionCard } from "./subscriptionCard/subscriptionCard";
