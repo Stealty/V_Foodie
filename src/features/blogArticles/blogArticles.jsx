2d27dc6a10c39aa506e97b4fdec242032ed30c61
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-06-24 11:44:33 -0300

    Fix/ importaçao repetida e form type onchange

--- src/features/blogArticles/blogArticles.jsx
+++ src/features/blogArticles/blogArticles.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./blogArticles.module.scss";
 import { ArticleFilter, Articles } from "@/components";
-import { ArticleFilter, Articles, RecipeCard, CardStack } from "@/components";
-
 import { useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useEffect } from "react";
@@ -18,38 +16,14 @@ const BlogArticles = () => {
 
   const handleSearch = (event) => {
     event.preventDefault();
-    const searchValue = event.target.value.toLowerCase();
 
+    const searchValue = event.target.value.toLowerCase();
     const filter = data.filter((article) =>
       article.articleTitle.toLowerCase().includes(searchValue)
     );
 
     setArticles(filter);
   };
-
-  const recipes = [
-    {
-      id: 1,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYOU8KqNFnoM3XOAT0bR66229KlXDTzeIPSw&usqp=CAU",
-      title: "Crochet Projects for Noodle Lovers",
-      author: "Wade Warren",
-    },
-    {
-      id: 2,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYOU8KqNFnoM3XOAT0bR66229KlXDTzeIPSw&usqp=CAU",
-      title: "Crochet Projects for Noodle Lovers",
-      author: "Wade Warren",
-    },
-    {
-      id: 3,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYOU8KqNFnoM3XOAT0bR66229KlXDTzeIPSw&usqp=CAU",
-      title: "Crochet Projects for Noodle Lovers",
-      author: "Wade Warren",
-    },
-  ];
 
   return (
     <>
@@ -66,14 +40,7 @@ const BlogArticles = () => {
           <Articles articles={articles} loading={loading} />
         </div>
         <div className={styles["blogArticles__recipesStack"]}>
-          <CardStack>
-            <h2 className={styles["blogArticles__recipesStack__title"]}>
-              Tasty Recipes
-            </h2>
-            {recipes.map((recipe) => (
-              <RecipeCard key={recipe.id} {...recipe}></RecipeCard>
-            ))}
-          </CardStack>
+          Recipes Stack
         </div>
         <div className={styles["blogArticles__ads"]}>Ads</div>
         <div className={styles["blogArticles__pageSelector"]}>
