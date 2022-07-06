58d4d9c294b8d7bde57857db0d49fc7f40434ef7
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-06 09:37:58 -0300

    feat(chefsection): Add components to chef section

--- src/components/organisms/chefSection/chefSection.jsx
+++ src/components/organisms/chefSection/chefSection.jsx
@@ -1,5 +1,18 @@
+import { Heading, Paragraph, SubmitButton } from "@atoms";
+import { ChefIllustration } from "@molecules";
+
 const ChefSection = () => {
-  return <div>ChefSection</div>;
+  return (
+    <div className="chefSection__Wrapper">
+      <Heading level="2">Everyone can be a chef in their own kitchen</Heading>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+      </Paragraph>
+      <SubmitButton text="Learn More" type="button" />
+      <ChefIllustration />
+    </div>
+  );
 };
 
 export default ChefSection;
