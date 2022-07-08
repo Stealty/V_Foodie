76a67dd1ed7d3e28435cf0f1ca5b7970baa52213
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-08 13:43:24 -0300

    feat(recipesGrid): Add desktop layout

--- src/components/molecules/recipesGrid/recipesGrid.jsx
+++ src/components/molecules/recipesGrid/recipesGrid.jsx
@@ -1,18 +1,19 @@
 import { useData } from "@context/useDataContext";
 import Card from "../../organisms/Carousel/Card";
+import { SiteBanner } from "@molecules";
+import styles from "./recipesGrid.module.scss";
 
 const RecipesGrid = () => {
   const { data, loading } = useData();
 
   return (
     <div>
-      <ul>
+      <ul className={styles["recipesGrid__list"]}>
         {!loading &&
-          data.map((recipe) => (
-            <li key={recipe.id}>
-              <Card card={recipe}></Card>
-            </li>
-          ))}
+          data.map((recipe) => <Card key={recipe.id} card={recipe}></Card>)}
+        <div className={styles["recipesGrid__banner"]}>
+          <SiteBanner />
+        </div>
       </ul>
     </div>
   );
