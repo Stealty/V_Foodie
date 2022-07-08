d88fe8e2a15fa0e2a68726aacfc31f912df3ceac
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-07-08 13:51:41 -0300

    Creating conditional render function and fixing styling

--- src/components/molecules/instaCard/instaCard.jsx
+++ src/components/molecules/instaCard/instaCard.jsx
@@ -4,15 +4,16 @@ import InstaCardMedia from "../instaCardMedia/instaCardMedia";
 import styles from "./instaCard.module.scss";
 
 const InstaCard = (props) => {
-  console.log(props);
+  /* console.log(props.postInfo.id); */
   return (
     <a
-      href={props.postInfo.permaLink}
-      target="_blank"
       className={styles.cardContainer}
+      href={props.postInfo.permalink}
+      target="_blank"
     >
       <InstaCardHeader username={props.postInfo.username} />
       <InstaCardMedia
+        mediaId={props.postInfo.id}
         mediaType={props.postInfo.media_type}
         mediaLink={props.postInfo.media_url}
       />
