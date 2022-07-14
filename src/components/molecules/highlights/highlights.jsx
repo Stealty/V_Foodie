fd9e5146e572f9879a2f5ffdeb582b91ce88a913
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-07-14 11:06:01 -0300

    feat/ scrolling for carousel in desktop version

--- src/components/molecules/highlights/highlights.jsx
+++ src/components/molecules/highlights/highlights.jsx
@@ -6,11 +6,11 @@ export default function Highlights() {
   const cards = [1, 2, 3, 4, 5];
   return (
     <div className={styles.highlights}>
-      <Carousel>
+      <Carousel scrolling={true} withArrows={false}>
         {cards.map((card) => {
           return (
-            <li className={styles.highlights__item}>
-              <HighlightCard key={card} />
+            <li className={styles.highlights__item} key={card}>
+              <HighlightCard />
             </li>
           );
         })}
